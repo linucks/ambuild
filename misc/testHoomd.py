d410c1051a503bd2130c736379a9a4ae6b065ee3
linucks <linucks42@gmail.com>
2014-09-13 14:36:37 +0100

    New test for all-optimisation

--- misc/testHoomd.py
+++ misc/testHoomd.py
@@ -1,37 +1,440 @@
+s="""<hoomd_xml version="1.4"><configuration timestep="0"><box Lx="30.0" Ly="30.0" Lz="30.0" /><position>
+1.368000031 0.750999987 -0.00383333316667
+0.034000002 1.559999943 0.00816666683333
+-1.333999991 0.810000002 -0.000833333166666
+-1.368000031 -0.750999987 -0.00383333316667
+-0.034000002 -1.559999943 0.00816666683333
+1.333999991 -0.810000002 -0.000833333166666
+2.357000113 -1.429999948 -0.00683333316667
+-0.059999999 -2.756000042 0.0201666668333
+-2.417000055 -1.325999975 -0.0168333341667
+0.059999999 2.756000042 0.0201666668333
+2.417000055 1.325999975 -0.0168333341667
+-5.83161401721 -0.968348368413 -0.861956287102
+-5.92532316944 -0.0427032182202 0.191349256977
+-4.82557355262 0.764933842627 0.485650553973
+-3.64647868969 0.698846081837 -0.257927804087
+-3.57599993313 -0.182064259365 -1.35066868559
+-4.66413435247 -1.01206872401 -1.62525231449
+-2.36411234942 -0.235509461783 -2.0813709141
+-2.531261961 1.53561316742 -0.00785539666939
+-6.96570544252 -1.76876842143 -1.14406990641
+-7.11761010742 -0.0270030097094 0.955325050401
+-4.88965550442 1.46423923594 1.31668039754
+-4.60005240067 -1.71137411732 -2.45628215805
+-2.03624564772 0.692180945161 -2.33059971022
+-2.43340470659 -0.805209132024 -2.91401797292
+-7.33582328042 -2.20163796683 -0.303766512919
+-6.77973085325 -2.48063969707 -1.83719670057
+-7.14995073246 0.738411452137 1.61544250598
+-7.941300554 0.000115138492548 0.362855884725
+</position><image>
+0 0 0
+0 0 0
+0 0 0
+0 0 0
+0 0 0
+0 0 0
+0 0 0
+0 0 0
+0 0 0
+0 0 0
+0 0 0
+0 0 0
+0 0 0
+0 0 0
+0 0 0
+0 0 0
+0 0 0
+0 0 0
+0 0 0
+0 0 0
+0 0 0
+0 0 0
+0 0 0
+0 0 0
+0 0 0
+0 0 0
+0 0 0
+0 0 0
+0 0 0
+</image><charge>
+0.396
+0.396
+0.396
+0.396
+0.396
+0.396
+-0.396
+-0.396
+-0.396
+-0.396
+-0.396
+0.083
+0.083
+-0.127
+0.083
+0.083
+-0.127
+-0.58
+-0.58
+-0.58
+-0.58
+0.127
+0.127
+0.249
+0.249
+0.249
+0.249
+0.249
+0.249
+</charge><diameter>
+0.1
+0.1
+0.1
+0.1
+0.1
+0.1
+0.1
+0.1
+0.1
+0.1
+0.1
+0.1
+0.1
+0.1
+0.1
+0.1
+0.1
+0.1
+0.1
+0.1
+0.1
+0.1
+0.1
+0.1
+0.1
+0.1
+0.1
+0.1
+0.1
+</diameter><type>
+c_0
+c_0
+c_0
+c_0
+c_0
+c_0
+o_1
+o_1
+o_1
+o_1
+o_1
+cpa
+cpa
+cp
+cpa
+cpa
+cp
+nb
+nb
+nb
+nb
+hc
+hc
+hn
+hn
+hn
+hn
+hn
+hn
+</type><mass>
+12.0107
+12.0107
+12.0107
+12.0107
+12.0107
+12.0107
+15.9994
+15.9994
+15.9994
+15.9994
+15.9994
+12.0107
+12.0107
+12.0107
+12.0107
+12.0107
+12.0107
+14.0067
+14.0067
+14.0067
+14.0067
+1.00794
+1.00794
+1.00794
+1.00794
+1.00794
+1.00794
+1.00794
+1.00794
+</mass><bond>
+c_0-c_0 0 1
+c_0-c_0 0 5
+c_0-o_1 0 10
+c_0-o_1 1 9
+c_0-c_0 1 2
+c_0-c_0 2 3
+c_0-c_0 3 4
+c_0-o_1 3 8
+c_0-o_1 4 7
+c_0-c_0 4 5
+c_0-o_1 5 6
+cpa-cp 11 16
+cpa-nb 11 19
+cpa-cpa 11 12
+cpa-nb 12 20
+cpa-cp 12 13
+cp-cpa 13 14
+cp-hc 13 21
+cpa-nb 14 18
+cpa-cpa 14 15
+cpa-cp 15 16
+cpa-nb 15 17
+cp-hc 16 22
+nb-hn 17 23
+nb-hn 17 24
+nb-hn 19 25
+nb-hn 19 26
+nb-hn 20 28
+nb-hn 20 27
+c_0-nb 2 18
+</bond><angle>
+c_0-c_0-c_0 0 1 2
+c_0-c_0-o_1 0 1 9
+c_0-c_0-c_0 0 5 4
+c_0-c_0-o_1 0 5 6
+c_0-c_0-c_0 1 0 5
+c_0-c_0-o_1 1 0 10
+c_0-c_0-c_0 1 2 3
+c_0-c_0-nb 1 2 18
+c_0-c_0-o_1 2 1 9
+c_0-c_0-c_0 2 3 4
+c_0-c_0-o_1 2 3 8
+c_0-nb-cpa 2 18 14
+c_0-c_0-nb 3 2 18
+c_0-c_0-c_0 3 4 5
+c_0-c_0-o_1 3 4 7
+c_0-c_0-o_1 4 3 8
+c_0-c_0-o_1 4 5 6
+c_0-c_0-o_1 5 0 10
+c_0-c_0-o_1 5 4 7
+cpa-cpa-cp 11 12 13
+cpa-cpa-nb 11 12 20
+cpa-cp-cpa 11 16 15
+cpa-cp-hc 11 16 22
+cpa-nb-hn 11 19 25
+cpa-nb-hn 11 19 26
+cpa-cpa-cp 12 11 16
+cpa-cpa-nb 12 11 19
+cpa-cp-cpa 12 13 14
+cpa-cp-hc 12 13 21
+cpa-nb-hn 12 20 27
+cpa-nb-hn 12 20 28
+cp-cpa-nb 13 12 20
+cp-cpa-cpa 13 14 15
+cp-cpa-nb 13 14 18
+cpa-cp-hc 14 13 21
+cpa-cpa-cp 14 15 16
+cpa-cpa-nb 14 15 17
+cpa-cpa-nb 15 14 18
+cpa-cp-hc 15 16 22
+cpa-nb-hn 15 17 23
+cpa-nb-hn 15 17 24
+cp-cpa-nb 16 11 19
+cp-cpa-nb 16 15 17
+hn-nb-hn 23 17 24
+hn-nb-hn 25 19 26
+hn-nb-hn 27 20 28
+</angle><dihedral>
+c_0-c_0-c_0-c_0 0 1 2 3
+c_0-c_0-c_0-nb 0 1 2 18
+c_0-c_0-c_0-c_0 0 5 4 3
+c_0-c_0-c_0-o_1 0 5 4 7
+c_0-c_0-c_0-c_0 1 0 5 4
+c_0-c_0-c_0-o_1 1 0 5 6
+c_0-c_0-c_0-c_0 1 2 3 4
+c_0-c_0-c_0-o_1 1 2 3 8
+c_0-c_0-nb-cpa 1 2 18 14
+c_0-c_0-c_0-c_0 2 1 0 5
+c_0-c_0-c_0-o_1 2 1 0 10
+c_0-c_0-c_0-c_0 2 3 4 5
+c_0-c_0-c_0-o_1 2 3 4 7
+c_0-nb-cpa-cp 2 18 14 13
+c_0-nb-cpa-cpa 2 18 14 15
+c_0-c_0-c_0-o_1 3 2 1 9
+c_0-c_0-nb-cpa 3 2 18 14
+c_0-c_0-c_0-o_1 3 4 5 6
+c_0-c_0-c_0-nb 4 3 2 18
+c_0-c_0-c_0-o_1 4 5 0 10
+c_0-c_0-c_0-o_1 5 0 1 9
+c_0-c_0-c_0-o_1 5 4 3 8
+o_1-c_0-c_0-o_1 6 5 0 10
+o_1-c_0-c_0-o_1 6 5 4 7
+o_1-c_0-c_0-o_1 7 4 3 8
+o_1-c_0-c_0-nb 8 3 2 18
+o_1-c_0-c_0-o_1 9 1 0 10
+o_1-c_0-c_0-nb 9 1 2 18
+cpa-cpa-cp-cpa 11 12 13 14
+cpa-cpa-cp-hc 11 12 13 21
+cpa-cpa-nb-hn 11 12 20 27
+cpa-cpa-nb-hn 11 12 20 28
+cpa-cp-cpa-cpa 11 16 15 14
+cpa-cp-cpa-nb 11 16 15 17
+cpa-cpa-cp-cpa 12 11 16 15
+cpa-cpa-cp-hc 12 11 16 22
+cpa-cpa-nb-hn 12 11 19 25
+cpa-cpa-nb-hn 12 11 19 26
+cpa-cp-cpa-cpa 12 13 14 15
+cpa-cp-cpa-nb 12 13 14 18
+cp-cpa-cpa-cp 13 12 11 16
+cp-cpa-cpa-nb 13 12 11 19
+cp-cpa-nb-hn 13 12 20 27
+cp-cpa-nb-hn 13 12 20 28
+cp-cpa-cpa-cp 13 14 15 16
+cp-cpa-cpa-nb 13 14 15 17
+cpa-cp-cpa-nb 14 13 12 20
+cpa-cpa-cp-hc 14 15 16 22
+cpa-cpa-nb-hn 14 15 17 23
+cpa-cpa-nb-hn 14 15 17 24
+cpa-cpa-cp-hc 15 14 13 21
+cpa-cp-cpa-nb 15 16 11 19
+cp-cpa-cpa-nb 16 11 12 20
+cp-cpa-nb-hn 16 11 19 25
+cp-cpa-nb-hn 16 11 19 26
+cp-cpa-cpa-nb 16 15 14 18
+cp-cpa-nb-hn 16 15 17 23
+cp-cpa-nb-hn 16 15 17 24
+nb-cpa-cpa-nb 17 15 14 18
+nb-cpa-cp-hc 17 15 16 22
+nb-cpa-cp-hc 18 14 13 21
+nb-cpa-cpa-nb 19 11 12 20
+nb-cpa-cp-hc 19 11 16 22
+nb-cpa-cp-hc 20 12 13 21
+</dihedral></configuration></hoomd_xml>"""
+
+import sys
+sys.path.append("/Applications/HOOMD-blue.app/Contents/lib/hoomd/python-module")
+sys.path.append("/Applications/HOOMD-blue.app/Contents/MacOS")
 from hoomd_script import *
 
-xmlFilename="hoomdOpt.xml"
+xmlFilename="test.xml"
+with open(xmlFilename,'w') as f:
+    f.writelines(s)
+
 system = init.read_xml( filename=xmlFilename )
 
-harmonic = bond.harmonic()
-harmonic.bond_coeff.set( 'cp-cp',  k=1550.0, r0=1.384 )
 
-dharmonic = dihedral.harmonic()
-dharmonic.set_coeff( 'c-cp-cp-c', k=30, d=-1, n=2  )
+
+
+
+
+
+bondh = bond.harmonic()
+bondh.bond_coeff.set( 'c_0-c_0',  k=970.0, r0=1.56 )
+bondh.bond_coeff.set( 'c_0-o_1',  k=970.0, r0=1.196 )
+bondh.bond_coeff.set( 'cpa-cp',  k=1550.0, r0=1.384 )
+bondh.bond_coeff.set( 'cpa-nb',  k=1200.0, r0=1.416 )
+bondh.bond_coeff.set( 'cpa-cpa',  k=1550.0, r0=1.384 )
+bondh.bond_coeff.set( 'cp-cpa',  k=1550.0, r0=1.384 )
+bondh.bond_coeff.set( 'cp-hc',  k=1400.0, r0=1.079 )
+bondh.bond_coeff.set( 'nb-hn',  k=1200.0, r0=1.015 )
+bondh.bond_coeff.set( 'c_0-nb',  k=1200.0, r0=1.32 )
+
+
+angleh=angle.harmonic()
+angleh.set_coeff( 'c_0-c_0-c_0',  k=1000.0, t0=2.09439510239 )
+angleh.set_coeff( 'c_0-c_0-o_1',  k=1000.0, t0=2.09439510239 )
+angleh.set_coeff( 'c_0-c_0-nb',  k=1000.0, t0=2.05948851735 )
+angleh.set_coeff( 'c_0-nb-cpa',  k=800.0, t0=2.40855436775 )
+angleh.set_coeff( 'cpa-cpa-cp',  k=800.0, t0=2.09439510239 )
+angleh.set_coeff( 'cpa-cpa-nb',  k=800.0, t0=2.09439510239 )
+angleh.set_coeff( 'cpa-cp-cpa',  k=800.0, t0=2.09439510239 )
+angleh.set_coeff( 'cpa-cp-hc',  k=800.0, t0=2.09439510239 )
+angleh.set_coeff( 'cpa-nb-hn',  k=800.0, t0=1.95476876223 )
+angleh.set_coeff( 'cp-cpa-nb',  k=800.0, t0=2.09439510239 )
+angleh.set_coeff( 'cp-cpa-cpa',  k=800.0, t0=2.09439510239 )
+angleh.set_coeff( 'hn-nb-hn',  k=800.0, t0=1.91288086019 )
+
+
+dihedralh = dihedral.harmonic()
+dihedralh.set_coeff('c_0-c_0-c_0-c_0',  k=30, d=-1, n=2)
+dihedralh.set_coeff('c_0-c_0-c_0-nb',  k=30, d=-1, n=2)
+dihedralh.set_coeff('c_0-c_0-c_0-o_1',  k=30, d=-1, n=2)
+dihedralh.set_coeff('c_0-c_0-nb-cpa',  k=30, d=-1, n=2)
+dihedralh.set_coeff('c_0-nb-cpa-cp',  k=30, d=-1, n=2)
+dihedralh.set_coeff('c_0-nb-cpa-cpa',  k=70, d=-1, n=2)
+dihedralh.set_coeff('o_1-c_0-c_0-o_1',  k=30, d=-1, n=2)
+dihedralh.set_coeff('o_1-c_0-c_0-nb',  k=30, d=-1, n=2)
+dihedralh.set_coeff('cpa-cpa-cp-cpa',  k=30, d=-1, n=2)
+dihedralh.set_coeff('cpa-cpa-cp-hc',  k=3, d=-1, n=2)
+dihedralh.set_coeff('cpa-cpa-nb-hn',  k=3, d=-1, n=2)
+dihedralh.set_coeff('cpa-cp-cpa-cpa',  k=30, d=-1, n=2)
+dihedralh.set_coeff('cpa-cp-cpa-nb',  k=30, d=-1, n=2)
+dihedralh.set_coeff('cp-cpa-cpa-cp',  k=30, d=-1, n=2)
+dihedralh.set_coeff('cp-cpa-cpa-nb',  k=30, d=-1, n=2)
+dihedralh.set_coeff('cp-cpa-nb-hn',  k=3, d=-1, n=2)
+dihedralh.set_coeff('nb-cpa-cpa-nb',  k=30, d=-1, n=2)
+dihedralh.set_coeff('nb-cpa-cp-hc',  k=3, d=-1, n=2)
+
 
 lj = pair.lj( r_cut=5.0)
-lj.pair_coeff.set( 'c', 'c', epsilon=0.0933, sigma=3.7736 )
-lj.pair_coeff.set( 'c', 'cp', epsilon=0.1016, sigma=3.7736 )
-lj.pair_coeff.set( 'c', 'br', epsilon=0.1016, sigma=3.7736 )
+lj.pair_coeff.set( 'c_0', 'c_0', epsilon=0.12, sigma=3.308 )
+lj.pair_coeff.set( 'c_0', 'o_1', epsilon=0.1755, sigma=3.4309 )
+lj.pair_coeff.set( 'c_0', 'cpa', epsilon=0.1068, sigma=3.5782 )
+lj.pair_coeff.set( 'c_0', 'cp', epsilon=0.1068, sigma=3.5782 )
+lj.pair_coeff.set( 'c_0', 'nb', epsilon=0.0736, sigma=3.7823 )
+lj.pair_coeff.set( 'c_0', 'hc', epsilon=0.0469, sigma=3.1707 )
+lj.pair_coeff.set( 'c_0', 'hn', epsilon=0.0029, sigma=2.9477 )
+lj.pair_coeff.set( 'o_1', 'o_1', epsilon=0.267, sigma=3.535 )
+lj.pair_coeff.set( 'o_1', 'cpa', epsilon=0.1686, sigma=3.664 )
+lj.pair_coeff.set( 'o_1', 'cp', epsilon=0.1686, sigma=3.664 )
+lj.pair_coeff.set( 'o_1', 'nb', epsilon=1.1208, sigma=2.32 )
+lj.pair_coeff.set( 'o_1', 'hc', epsilon=0.0649, sigma=3.3189 )
+lj.pair_coeff.set( 'o_1', 'hn', epsilon=0.0035, sigma=1.611 )
+lj.pair_coeff.set( 'cpa', 'cpa', epsilon=0.1106, sigma=3.7736 )
+lj.pair_coeff.set( 'cpa', 'cp', epsilon=0.1106, sigma=3.7736 )
+lj.pair_coeff.set( 'cpa', 'nb', epsilon=0.0827, sigma=3.9357 )
+lj.pair_coeff.set( 'cpa', 'hc', epsilon=0.1106, sigma=3.7736 )
+lj.pair_coeff.set( 'cpa', 'hn', epsilon=0.0019, sigma=3.3622 )
 lj.pair_coeff.set( 'cp', 'cp', epsilon=0.1106, sigma=1.7736 )
-lj.pair_coeff.set( 'cp', 'br', epsilon=0.1106, sigma=3.7736 )
-lj.pair_coeff.set( 'br', 'br', epsilon=0.0376, sigma=3.4893 )
+lj.pair_coeff.set( 'cp', 'nb', epsilon=0.0827, sigma=3.9357 )
+lj.pair_coeff.set( 'cp', 'hc', epsilon=0.0376, sigma=3.4893 )
+lj.pair_coeff.set( 'cp', 'hn', epsilon=0.0019, sigma=3.3622 )
+lj.pair_coeff.set( 'nb', 'nb', epsilon=0.065, sigma=4.07 )
+lj.pair_coeff.set( 'nb', 'hc', epsilon=0.0248, sigma=3.7161 )
+lj.pair_coeff.set( 'nb', 'hn', epsilon=0.0011, sigma=3.6262 )
+lj.pair_coeff.set( 'hc', 'hc', epsilon=0.1106, sigma=3.7736 )
+lj.pair_coeff.set( 'hc', 'hn', epsilon=0.0016, sigma=2.6693 )
+lj.pair_coeff.set( 'hn', 'hn', epsilon=0.013, sigma=1.098 )
+
+
 
 globals.neighbor_list.reset_exclusions(exclusions = ['1-2', '1-3', '1-4', 'angle', 'body'] )
 
-hlog = analyze.log(filename='mylog.csv',
-                   quantities=[
-                               'num_particles',
-                               'pair_lj_energy',
-                               'potential_energy',
-                               'kinetic_energy',
-                               ],
-                   period=100,
-                   header_prefix='#',
-                   overwrite=True
-                   )
+# hlog = analyze.log(filename='mylog.csv',
+#                    quantities=[
+#                                'num_particles',
+#                                'pair_lj_energy',
+#                                'potential_energy',
+#                                'kinetic_energy',
+#                                ],
+#                    period=100,
+#                    header_prefix='#',
+#                    overwrite=True
+#                    )
 
-fire = integrate.mode_minimize_rigid_fire( group=group.all(),
+fire = integrate.mode_minimize_fire( group=group.all(),
                                            dt=0.005,
                                            Nmin=5,
                                            alpha_start=0.1,
@@ -39,16 +442,20 @@ fire = integrate.mode_minimize_rigid_fire( group=group.all(),
                                            Etol=1e-4,
                                            finc=1.1,
                                            fdec=0.5 )
-                                           
-xmld = dump.xml(filename="runopt.xml", vis=True)
-dcdd = dump.dcd(filename="runopt.dcd",
-               period=1, 
+
+xmld = dump.xml(filename="runtest.xml", vis=True)
+dcdd = dump.dcd(filename="runtest.dcd",
+               period=1,
                unwrap_full=True,
                overwrite=True,
                 )
 
-for i in range(5):
-     run( 100000 )
-     if fire.has_converged():
-         break
- 
+
+optCycles=10000
+run(optCycles,
+    callback=lambda x: -1 if fire.has_converged() else 0,
+    callback_period=1 )
+
+if fire.has_converged():
+    print "CONVERGED!!!!"
+
