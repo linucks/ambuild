ab9b60fe98e5da5394186336caac93ae47ab44e9
linucks <linucks42@gmail.com>
2016-05-14 20:06:16 +0100

    Script to run all tests

---
+++ builder/test.py
@@ -0,0 +1,20 @@
+#!/usr/bin/env python
+'''
+Created on 14 May 2016
+
+@author: jmht
+'''
+
+import os
+import sys
+import unittest
+from paths import AMBUILD_DIR
+
+VERBOSITY = 2
+suite = unittest.TestLoader().discover(AMBUILD_DIR)
+if int(suite.countTestCases()) <= 0:
+    msg = 'Could not find any tests to run in directory: {0}'.format(AMBUILD_DIR) + os.linesep
+    sys.stderr.write(msg)
+    sys.exit(1)
+
+unittest.TextTestRunner(verbosity=VERBOSITY, buffer=buffer).run(suite)
