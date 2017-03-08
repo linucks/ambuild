0f2c028773869cc3df520ce6d108b2bd95e164cc
linucks <linucks42@gmail.com>
2017-03-08 09:57:35 +0000

    Updated to deal with when there is no bond and to return True/False based on whether we did anything

--- standard_inputs/paf_catalyst_script.py
+++ standard_inputs/paf_catalyst_script.py
@@ -25,13 +25,14 @@ mycell.addBondType( 'PAF:a-cat:a' )
 
 def unbondCat(mycell):
     """Function to unbond a Ni-catalyst bonded to two PAF groups"""
-    
-    if not mycell.lastAdded: return
-    
+
+    if not mycell.lastAdded: return False
     lblock = mycell.blocks[mycell.lastAdded]
+    
+    if not len(lblock._blockBonds): False
     # Last added bond is always last in the list
     bond = lblock._blockBonds[-1]
-    
+
     # See if either of the blocks connected is the cat
     t1 = bond.endGroup1.type()
     t2 = bond.endGroup2.type()
@@ -40,8 +41,8 @@ def unbondCat(mycell):
         cfrag = bond.endGroup1.fragment
     elif t2 == 'cat:a':
         cfrag = bond.endGroup2.fragment
-    else: return # Nothing to do
-    
+    else: return False# Nothing to do
+
     # The block has multiple fragments, but we are only interested in this cat fragment
     # and if this has two bonds made to it.
     # For time being assume only two allowed bonds to cat
@@ -49,9 +50,9 @@ def unbondCat(mycell):
     if not all([ eg.bonded for eg in endGroups ]): return
     assert len(endGroups) == 2, "Assumption is cat only has 2 endGroups!"
     eg1, eg2 = endGroups
-    
+
     # Both are bonded so we now need to go through each bond in the block in turn and unbond the blocks
-    
+
     # Get two bonds in this block that bond it to PAF and also the two paf endgroups
     b1 = None
     b2 = None
@@ -75,29 +76,29 @@ def unbondCat(mycell):
             else:
                 b1 = bond
                 paf1Eg = bond.endGroup1
-    
+
     assert b1 and b2,"Could not find both bonds"
-    
+
     # Remove the block from the cell
     mycell.delBlock(lblock.id)
-    
+
     # Break the two bonds
     paf1 = lblock.deleteBond(b1)
     #print "DELETED PAF1 ",paf1.id
+ 
     paf2 = lblock.deleteBond(b2)
     #print "DELETED PAF2 ",paf2.id
-    
+
     # Add the unbonded blocks back to the cell
     mycell.addBlock(lblock)
     mycell.addBlock(paf1)
     mycell.addBlock(paf2)
-    
+
     # We now need to bond the two PAF groups
     assert paf1Eg.free() and paf2Eg.free(),"PAF endgroups aren't free!"
     bond = buildingBlock.Bond(paf1Eg, paf2Eg)
     mycell.bondBlock(bond)
-    
-    return
+    return True
 
 mycell.seed( 1, fragmentType='cat', center=True)
 mycell.growBlocks(toGrow=1, cellEndGroups=None, libraryEndGroups='PAF:a', maxTries=500)
