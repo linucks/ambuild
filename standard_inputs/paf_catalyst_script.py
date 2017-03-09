af60eaa6b5e674ec67942050a43a9c15975d5b4f
linucks <linucks42@gmail.com>
2017-03-09 21:18:47 +0000

    Updated for unbondCat being part of cell and added onbond function for changing the endGroup type on bonding

--- standard_inputs/paf_catalyst_script.py
+++ standard_inputs/paf_catalyst_script.py
@@ -1,5 +1,32 @@
 #!/usr/bin/env python
 
+"""
+1. addBondType('PAF:a-cat:a' ) - PAFs can bond to the free catalyst
+2. addBondType('cat:a*-cat:a*' )  - catalysts with PAFs bonded to them can bond to each other
+3. When PAF:a has bonded to cat:a, we change the bond type of cat:a  to cat:a*
+4. When two catalysts with PAF bonded to them form a bond, we unbond the catalysts from the two PAF
+   blocks and and bond the PAF blocks to each other
+
+Questions
+Do we do a multi-step operation - e.g. bond the catalysts together and then remove one and join the PAFS
+ - or, as soon as two PAF-bonded cat groups can bond, we delete the two PAF/cat bonds and directly bond
+ the PAF units. What happens to the catalysts? Presuamably we just leave them? 
+ 
+ 
+ 
+ If the last bond was between two cat:a* groups:
+     get the 4 fragments 2*cat & 2*paf
+     unbond all 4, but keep track of the two paf endGroups
+     bond the two paf endgroups
+ 
+ 
+ MISC
+ Fix debugging so it's easier to turn on and off in a standard way
+ 
+ 
+
+"""
+
 import sys
 sys.path.append("/opt/ambuild/builder")
 
@@ -19,92 +46,30 @@ boxDim=[40,40,40]
 mycell = cell.Cell(boxDim, atomMargin=0.1, bondMargin=0.5, bondAngleMargin=5, doLog=False )
 
 #import the two fragment files if you have 2 different building blocks
-mycell.libraryAddFragment( filename='PAF.car', fragmentType='PAF' )
+mycell.libraryAddFragment( filename='/opt/ambuild/blocks/PAF.car', fragmentType='PAF' )
 mycell.libraryAddFragment( filename='pafh2.car', fragmentType='cat' )
+#mycell.libraryAddFragment( filename='/opt/ambuild/blocks/ch4.car', fragmentType='cat' )
 mycell.addBondType( 'PAF:a-cat:a' )
 
-def unbondCat(mycell):
-    """Function to unbond a Ni-catalyst bonded to two PAF groups"""
+def onbond(endGroup):
+    """Append * to all endGroups that match"""
+    if not endGroup.type() == 'cat:a': return
+    frag = endGroup.fragment
+    for eg in frag.endGroups():
+        assert not eg._endGroupType.endswith('*'),"Already got bonded endGroup"
+        eg._endGroupType += '*'
+    return
 
-    if not mycell.lastAdded: return False
-    lblock = mycell.blocks[mycell.lastAdded]
-    
-    if not lblock._blockBonds or not len(lblock._blockBonds): return False
-    # Last added bond is always last in the list
-    bond = lblock._blockBonds[-1]
-
-    # See if either of the blocks connected is the cat
-    t1 = bond.endGroup1.type()
-    t2 = bond.endGroup2.type()
-    cfrag = None
-    if t1 == 'cat:a':
-        cfrag = bond.endGroup1.fragment
-    elif t2 == 'cat:a':
-        cfrag = bond.endGroup2.fragment
-    else: return False# Nothing to do
-
-    # The block has multiple fragments, but we are only interested in this cat fragment
-    # and if this has two bonds made to it.
-    # For time being assume only two allowed bonds to cat
-    endGroups = cfrag.endGroups()
-    if not all([ eg.bonded for eg in endGroups ]): return
-    assert len(endGroups) == 2, "Assumption is cat only has 2 endGroups!"
-    eg1, eg2 = endGroups
-
-    # Both are bonded so we now need to go through each bond in the block in turn and unbond the blocks
-
-    # Get two bonds in this block that bond it to PAF and also the two paf endgroups
-    b1 = None
-    b2 = None
-    paf1Eg = None
-    paf2Eg = None
-    for bond in lblock._blockBonds:
-        #if bond.endGroup1 == eg1 or bond.endGroup2 == eg1 or bond.endGroup1 == eg2 or bond.endGroup2 == eg2:
-        if bond.endGroup1 in [eg1, eg2]:
-            assert not (b1 and b2), "More than 2 bonds linked to cat"
-            if b1:
-                b2 = bond
-                paf2Eg = bond.endGroup2
-            else:
-                b1 = bond
-                paf1Eg = bond.endGroup2
-        elif bond.endGroup2 in [eg1, eg2]:
-            assert not (b1 and b2), "More than 2 bonds linked to cat"
-            if b1:
-                b2 = bond
-                paf2Eg = bond.endGroup1
-            else:
-                b1 = bond
-                paf1Eg = bond.endGroup1
-
-    assert b1 and b2,"Could not find both bonds"
-
-    # Remove the block from the cell
-    mycell.delBlock(lblock.id)
-
-    # Break the two bonds
-    paf1 = lblock.deleteBond(b1)
-    #print "DELETED PAF1 ",paf1.id
- 
-    paf2 = lblock.deleteBond(b2)
-    #print "DELETED PAF2 ",paf2.id
-
-    # Add the unbonded blocks back to the cell
-    mycell.addBlock(lblock)
-    mycell.addBlock(paf1)
-    mycell.addBlock(paf2)
-
-    # We now need to bond the two PAF groups
-    assert paf1Eg.free() and paf2Eg.free(),"PAF endgroups aren't free!"
-    bond = buildingBlock.Bond(paf1Eg, paf2Eg)
-    mycell.bondBlock(bond)
-    return True
+# Use for changing endGroup type
+mycell.setBondingFunction('cat', onbond)
 
 mycell.seed( 1, fragmentType='cat', center=True)
 mycell.growBlocks(toGrow=1, cellEndGroups=None, libraryEndGroups='PAF:a', maxTries=500)
-unbondCat(mycell)
+if mycell.unbondCat():
+    print "unbondCat1"
 mycell.growBlocks(toGrow=1, cellEndGroups=None, libraryEndGroups='PAF:a', maxTries=500)
 mycell.dump()
-unbondCat(mycell)
+if mycell.unbondCat():
+    print "unbondCat2"
 mycell.dump()
 
