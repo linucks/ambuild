fb87eef8bd5fa1582b75835341037ea41ee19473
linucks <linucks42@gmail.com>
2018-03-04 21:22:25 +0000

    Calculate pincipal axes

--- builder/testFragment.py
+++ builder/testFragment.py
@@ -27,6 +27,30 @@ class Test(unittest.TestCase):
         for body in frag.bodies():
             I = body.momentInertia(dim=None, center=False, bodyspace=True)
             self.assertTrue(numpy.allclose(I, Iref))
+            # https://github.com/pierrepo/principal_axes/blob/master/principal_axes.py
+            
+            # calculate principal moments of inertia
+            e_values, e_vectors = numpy.linalg.eig(I)
+            # warning eigen values are not necessary ordered!
+            # http://docs.scipy.org/doc/numpy/reference/generated/numpy.linalg.eig.html
+            print("(Unordered) eigen values:")
+            print(e_values)
+            print("(Unordered) eigen vectors:")
+            print(e_vectors)
+        
+            #--------------------------------------------------------------------------
+            # order eigen values (and eigen vectors)
+            #
+            # axis1 is the principal axis with the biggest eigen value (eval1)
+            # axis2 is the principal axis with the second biggest eigen value (eval2)
+            # axis3 is the principal axis with the smallest eigen value (eval3)
+            #--------------------------------------------------------------------------
+            order = numpy.argsort(e_values)
+            eval3, eval2, eval1 = e_values[order]
+            axis3, axis2, axis1 = e_vectors[:, order].transpose()
+            print ("GOT ACXES, ", axis3, axis2, axis1)
+            print("Inertia axis are now ordered !")
+            
         pass
 
 
