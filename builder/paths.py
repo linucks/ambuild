a992ad51f2fd620797c0c43256a22e8deaa84d85
linucks <linucks42@gmail.com>
2018-06-17 14:18:25 +0100

    Removed old directory

--- builder/paths.py
+++
@@ -1,6 +0,0 @@
-#!/usr/bin/env python
-'''Paths to stuff we need'''
-import os
-AMBUILD_DIR = os.path.join(os.path.abspath(os.path.dirname(__file__)),'..')
-BLOCKS_DIR = os.path.join(AMBUILD_DIR, 'blocks')
-PARAMS_DIR = os.path.join(AMBUILD_DIR, 'params')
