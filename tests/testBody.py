c42e21a89252e7abebebebb51d98c47b63f0254e
linucks <linucks42@gmail.com>
2018-07-15 19:28:13 +0100

    Fix unittest

--- tests/testBody.py
+++ tests/testBody.py
@@ -28,27 +28,6 @@ class Test(unittest.TestCase):
         for i, b in enumerate(f1.bodies()):
             bstr = "{}{}{}".format(i, ftype, "0000")
             self.assertEqual(bstr, b.rigidConfigStr)
-    
-    def testOrientation1(self):
-        
-        class Cell(object):
-            """Mock Cell object for testing"""
-            def __init__(self, rigidParticleMgr):
-                self.rigidParticleMgr = rigidParticleMgr
-        
-        rigidParticleMgr = ab_rigidparticle.RigidParticleManager()
-        cell = Cell(rigidParticleMgr)
-        
-        ch4ca = os.path.join(BLOCKS_DIR, "ch4Ca2.car")
-        ftype = 'A'
-        f1 = ab_fragment.Fragment(filePath=ch4ca, fragmentType=ftype)
-        b1 = list(f1.bodies())[0]
-        
-        tref = "{}{}{}".format(0, ftype, "0000")
-        self.assertEqual(b1.rigidConfigStr, tref)
-        tref = "AA"
-        self.assertEqual(b1.rigidConfigStr, tref)
-        self.assertTrue(np.allclose(b1.orientation, np.array([1.0, 0.0, 0.0, 0.0]), atol=0.0001))
         
     def testPrincipalMoments(self):
         """Test principal moments don't change with rotation"""
