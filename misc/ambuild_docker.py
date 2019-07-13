de7c1461cf2581ab14b044978202c1f194aaf991
patrick <patrick@heasman@lancaster.ac.uk>
2019-07-13 19:50:17 +0100

    Restore deleted file

---
+++ misc/ambuild_docker.py
@@ -0,0 +1,17 @@
+#!/bin/bash
+/usr/local/bin/docker run \
+-it \
+--rm \
+--volume "$PWD":/home/glotzerlab \
+--volume /opt/ambuild.git/ambuild:/usr/lib/python3/dist-packages/ambuild \
+--volume /opt/ambuild.git/params:/home/glotzerlab/params \
+--volume /opt/ambuild.git/blocks:/home/glotzerlab/blocks \
+--volume /opt/ambuild.git/tests/test_data:/home/glotzerlab/test_data \
+--workdir /home/glotzerlab \
+glotzerlab/software \
+python3 $*
+
+#--volume $HOME:$HOME \
+#--runtime=nvidia \
+#--volume $HOME/Dropbox/Ambuild_Files/Parameters:$HOME/Dropbox/Ambuild_Files/Parameters \
+#--volume $HOME/Dropbox/Ambuild_Files/Blocks:$HOME/Dropbox/Ambuild_Files/Blocks \
