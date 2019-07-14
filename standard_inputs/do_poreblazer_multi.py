e461fa3a25bd30822af2938662c0bb31c4c626c8
jmht <linucks42@gmail.com>
2019-07-14 19:13:36 +0100

    Add multiprocessing functionality

--- standard_inputs/do_poreblazer_multi.py
+++ standard_inputs/do_poreblazer_multi.py
@@ -1,13 +1,25 @@
 #!/usr/bin/env python
 import glob
+import multiprocessing
 import sys
 sys.path.append("/opt/ambuild.git")
 from ambuild import ab_util
 
+# This script should be run in a directory containing ambuild pkl files (step_X.pkl.gz)
+# Each pkl file needs to be numbered differently (renaming the file won't help).
+
+# Edit these to suit your case
+num_processors = 6
 paramsDir = '/home/pierre/Dropbox/Ambuild_Files/Parameters'
 poreblazerExe = '/opt/poreblazer/src/poreblazer.exe'
-
-for spkl in sorted(glob.glob('step_*.pkl.gz')):
-    print('Processing pkl file: {} '.format(spkl))
-    mycell = ab_util.cellFromPickle(spkl, paramsDir=paramsDir)
+#
+# Don't change anything below here
+#
+def run_poreblazer(pkl_file):
+    print('Processing pkl file: {} '.format(pkl_file))
+    mycell = ab_util.cellFromPickle(pkl_file, paramsDir=paramsDir)
     mycell.poreblazer(poreblazerExe)
+
+pklfiles = glob.glob('step_*.pkl.gz')
+pool = multiprocessing.Pool(processes=num_processors)
+pool.map(run_poreblazer, pklfiles)
