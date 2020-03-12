be70fa71a974cb038acc8106e303791ca72d1210
jmht <linucks42@gmail.com>
2020-03-12 21:11:45 +0000

    Fixed to process files in the correct order

--- standard_inputs/cell_block_analysis.py
+++ standard_inputs/cell_block_analysis.py
@@ -7,16 +7,19 @@ import sys
 sys.path.append("/opt/ambuild.git")
 from ambuild import ab_util
 
-paramsDir = '/home/pierre/Dropbox/Ambuild_Files/Parameters'
+def pkl_step_num(pkl_filename):
+    return re.match('step_([0-9]+)\.pkl.*',pkl_filename).group(1)
 
+paramsDir = '/home/pierre/Dropbox/Ambuild_Files/Parameters'
 ignore = ['DMF', 'TEA', 'cat']
+
 prev_block_data = {}
 with open('blocks.csv', 'w') as csvfile:
     w = csv.writer(csvfile, delimiter=',')
     w.writerow(['nstep','nblocks','max_mass', 'zip_bonds'])
-    for p in sorted(glob.glob("*.pkl*")):
+    for p in sorted(glob.glob("*.pkl*"), key=pkl_step_num):
         print("Processing: {}".format(p))
-        nstep  = re.match('step_([0-9]+)\.pkl.*',p).group(1)
+        nstep  = pkl_step_num(p)
         try:
             mycell = ab_util.cellFromPickle(p, paramsDir=paramsDir)
         except EOFError:
@@ -29,18 +32,11 @@ with open('blocks.csv', 'w') as csvfile:
         for k, b in mycell.blocks.items():
             if len(b.fragments) == 1 and b.fragments[0].fragmentType in ignore:
                 continue
-
             current_block_data[k] = len(b.blockBonds())
             if k in prev_block_data and prev_block_data[k] < current_block_data[k]:
                 zip_bonds += current_block_data[k] - prev_block_data[k]
             nblocks += 1
             max_mass = max(max_mass, b.blockMass())
-        #print("SAME ",set(current_block_data.keys()).intersection(set(prev_block_data.keys())))
         prev_block_data = current_block_data
         w.writerow([nstep,nblocks,max_mass, zip_bonds])
 
-#with open('blocks.csv', 'w') as csvfile:
-#    w = csv.writer(csvfile, delimiter=',')
-#    w.writerow(['nstep','nblocks','max_mass', 'zip_bonds'])
-#    for d in data: 
-#        w.writerow(d)
