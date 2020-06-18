fc2d9bf92d572a57b6f9a03b80519d4ca4c22ffa
linucks <linucks42@gmail.com>
2020-06-18 16:47:55 +0100

    Update for argument handling

--- misc/ambuild_docker.py
+++ misc/ambuild_docker.py
@@ -1,17 +1,26 @@
 #!/bin/bash
+
+# Usage:
+# ambuild_docker.py <volume_arguments> script
+
+# Get root dir and script argumnts
+ambuild_dir="$( cd "$( dirname "${BASH_SOURCE[0]}" )/.." >/dev/null 2>&1 && pwd )"
+script=$1 
+extra_args=""
+if [ ${#} -ge 2 ]; then
+   script="${@:(-1):1}"
+   extra_args="${@:1:$(($#-1))}"
+fi
+
+# Run
 docker run \
 -it \
 --rm \
 --runtime=nvidia \
 --volume "$PWD":/home/glotzerlab \
---volume /opt/ambuild/ambuild:/usr/lib/python3/dist-packages/ambuild \
---volume /opt/ambuild/tests/params:/home/glotzerlab/params \
---volume /opt/ambuild/tests/blocks:/home/glotzerlab/blocks \
+--volume ${ambuild_dir}/ambuild:/usr/lib/python3/dist-packages/ambuild \
 --workdir /home/glotzerlab \
+$extra_args \
 glotzerlab/software \
-python3 $*
+python3 $script
 
-#--volume $HOME:$HOME \
-#--runtime=nvidia \
-#--volume $HOME/Dropbox/Ambuild_Files/Parameters:$HOME/Dropbox/Ambuild_Files/Parameters \
-#--volume $HOME/Dropbox/Ambuild_Files/Blocks:$HOME/Dropbox/Ambuild_Files/Blocks \
