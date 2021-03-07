370b07cd0170e34af4a52c31db79e634b8698994
linucks <linucks42@gmail.com>
2021-03-07 16:53:59 +0000

    Script to run ab_util.py under docker

---
+++ misc/run_ambuild_util_docker.sh
@@ -0,0 +1,19 @@
+#!/bin/bash
+
+# Usage:
+# ambuild_docker.py <volume_arguments> script
+
+# Get root dir and script argumnts
+ambuild_dir="$( cd "$( dirname "${BASH_SOURCE[0]}" )/.." >/dev/null 2>&1 && pwd )"
+
+# Run
+docker run \
+-it \
+--rm \
+--runtime=nvidia \
+--volume "$PWD":/home/glotzerlab \
+--volume ${ambuild_dir}/ambuild:/usr/lib/python3/dist-packages/ambuild \
+--workdir /home/glotzerlab \
+glotzerlab/software \
+python3 /usr/lib/python3/dist-packages/ambuild/ab_util.py $*
+
