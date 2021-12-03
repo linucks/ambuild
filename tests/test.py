c6a90cb13af686b56255260ae1b238302c0be078
jmht <linucks42@gmail.com>
2021-12-03 15:38:24 +0000

    Return return code

--- tests/test.py
+++ tests/test.py
@@ -23,4 +23,4 @@ if int(suite.countTestCases()) <= 0:
     sys.stderr.write(msg)
     sys.exit(1)
 
-return unittest.TextTestRunner(verbosity=VERBOSITY, buffer=False).run(suite)
+sys.exit(unittest.TextTestRunner(verbosity=VERBOSITY, buffer=False).run(suite))
