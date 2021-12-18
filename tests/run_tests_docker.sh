a1b389e78771aa9f52e84dce350562e4ef078f01
jmht <linucks42@gmail.com>
2021-12-18 18:05:52 +0000

    Updated script to run individual tests

--- tests/run_tests_docker.sh
+++ tests/run_tests_docker.sh
@@ -3,6 +3,15 @@
 run_dir=`pwd -P`
 ambuild_dir=$(cd $run_dir/..;pwd -P)
 
+# Get current user id
+uid=`id | awk -F "=" {'print $2'}| awk -F "(" {'print $1'}`
+
+args="python3 ./run_tests.py"
+# Below for runnining individual unittests e.g: ./run_tests_docker.sh python3 -m unittest testCell.Test.testCat1Paf2
+if [ $# -gt 0 ]; then
+    args=$*
+fi
+
 # Not sure if setting PYTHONPATH below is sensible. In the container it's set as
 # "/ignore/pythonpath" so I'm not sure if this is used by HOOMDBLUE internally
 # However it seems to work and deals with the problem that different container versions
@@ -17,6 +26,6 @@ docker run \
 --volume ${ambuild_dir}/ambuild:/usr/lib/python3/dist-packages/ambuild \
 --workdir $run_dir \
 --env PYTHONPATH=/usr/lib/python3/dist-packages \
---user 1001 \
+--user $uid \
 glotzerlab/software \
-python3 ./run_tests.py
+$args
